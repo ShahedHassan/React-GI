af7d53b2e69664addafbe62f683755bdcc0d76ad
Shade Hassan <Shade@MacBook-Pro-8.lan>
2022-07-19 22:27:50 -0500

    added basic info and debugged

--- reactshadegi/src/App.js
+++ reactshadegi/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';  
+import BasicInfo from './components/BasicInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let people = [
+  {
+    name: 'Shade',
+    phone: '2142351293',
+    age: 22,
+    favoriteFood: 'sushi'
+  },
+  {
+    name: 'Somesh',
+    phone: '1234567890',
+    age: 20,
+    favoriteFood: 'glizzys'
+  },
+  {
+    name: 'Matteo',
+    phone: '0987654321',
+    age: 25,
+    favoriteFood: 'babies'
+  },
+  {
+    name: 'Robert',
+    phone: '6574839201',
+    age: 26,
+    favoriteFood: 'Sugar'
+  },
+  {
+    name: 'Banjo',
+    phone: '0192837465',
+    age: 1,
+    favoriteFood: 'garbage'
+  }
+]
+
+function iteratePeople(arr) {
+  return arr.map((person) => {
+    return <BasicInfo person={person} />
+  }
+  )
 }
 
-export default App;
+export default class App extends Component {  
+  // easy
+  constructor(props){  
+    super(props);  
+    this.state = {
+        person: {
+        }
+    }
+  }  
+  // very easy/ medium
+    render() {
+      return (iteratePeople(people))
+  }
+}
